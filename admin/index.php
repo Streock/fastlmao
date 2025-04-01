6feb4cdb057c509933b3c0cc6b5e3bfba6683ab1
Streock <82071964+Streock@users.noreply.github.com>
2025-04-01 11:26:23 +0300

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,6 @@
+<meta http-equiv="refresh" content="0; url=/admin/appointments"> 
+<?php 
+//require $_SERVER['DOCUMENT_ROOT'].'/system/includes/Header.php';
+//require $_SERVER['DOCUMENT_ROOT'].'/system/includes/Home.php';
+//require $_SERVER['DOCUMENT_ROOT'].'/system/includes/Footer.php';
+?>
